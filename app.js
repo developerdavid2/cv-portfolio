1cc90992f86c572f200bbf81a0a06a86134a6a7c
David5183 <jacobsdavid.dr@gmail.com>
2023-05-24 01:59:01 +0100

    First commit
    This a CV Portfolio built in HTML, CSS and JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,17 @@
+function pageTransition() {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+
+     //Toggle theme
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })    
+}
+
+pageTransition();
